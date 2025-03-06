210b5d292e207ed62aa749061e76e75d853c674a
James A. Duncan <james@kinburnstreet.consulting>
2025-03-06 13:56:11 +0000

    just something to work for now

---
+++ index.mjs
@@ -0,0 +1,45 @@
+import { range, responseRange } from "jsr:@oak/commons/range";
+import { typeByExtension } from "jsr:@std/media-types/type-by-extension";
+import { extname } from "jsr:@std/path/extname";
+/*
+    this is borrowed from https://jsr.io/@oak/commons/doc/range for the time being
+*/
+export default async function( req, vhost ) {
+    const url = new URL(req.url);
+    const file = await Deno.open(`./static${url.pathname}`);
+    const fileInfo = await file.stat();
+    const headers = { "accept-ranges": "bytes", "content-type": type };
+    if (req.method === "HEAD") {
+        return new Response(null, {
+            headers: {
+            ...headers,
+            "content-length": String(fileInfo.size),
+            },
+        });
+    }
+    if (req.method === "GET") {
+        const result = await range(req, fileInfo);
+        if (result.ok) {
+            if (result.ranges) {
+                return responseRange(file, fileInfo.size, result.ranges, {
+                    headers,
+                }, { type });
+            } else {
+                return new Response(file.readable, {
+                    headers: {
+                        ...headers,
+                        "content-length": String(fileInfo.size),
+                    },
+                });
+            }
+        } else {
+            return new Response(null, {
+                status: 416,
+                statusText: "Range Not Satisfiable",
+                headers,
+            });
+        }
+    }
+    return new Response(null, { status: 405, statusText: "Method Not Allowed" });
+};
+
