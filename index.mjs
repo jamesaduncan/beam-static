d5e94c98eed96d2cfadba7863317324bc5226efa
James A. Duncan <james@kinburnstreet.consulting>
2025-03-06 15:57:35 +0000

    use the setting from the vhost.

--- index.mjs
+++ index.mjs
@@ -1,12 +1,14 @@
 import { range, responseRange } from "jsr:@oak/commons/range";
 import { typeByExtension } from "jsr:@std/media-types/type-by-extension";
 import { extname } from "jsr:@std/path/extname";
+import * as path from "jsr:@std/path";
+
 /*
     this is borrowed from https://jsr.io/@oak/commons/doc/range for the time being
 */
 export default async function( req, vhost ) {
     const url = new URL(req.url);
-    const file = await Deno.open(`./static${url.pathname}`);
+    const file = await Deno.open( path.join( vhost.root, url.pathname ) );
     const fileInfo = await file.stat();
     const headers = { "accept-ranges": "bytes", "content-type": type };
     if (req.method === "HEAD") {
